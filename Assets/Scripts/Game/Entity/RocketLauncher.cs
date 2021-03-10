beb78d80868985a0992a4636aecc4e54a3c725b2
Kurtis Knodel <kurtisknodel@gmail.com>
2021-03-10 09:20:52 -0800

    Gravity wells are no longer spammable

--- Assets/Scripts/Game/Entity/RocketLauncher.cs
+++ Assets/Scripts/Game/Entity/RocketLauncher.cs
@@ -8,14 +8,17 @@ public class RocketLauncher : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D playerBody;
     [SerializeField] private float rocketCooldown = 0.5f;
+    [SerializeField] private float gravityWellCooldown = 2.0f;
     [SerializeField] private float recoilStrength = 1;
     [SerializeField] private UnityEvent onShoot = new UnityEvent();
 
     private bool canFireRocket = true;
+    private bool canFireGravityWell = true;
     private Vector2 mouseDir = Vector2.right;
 
     private new Transform transform; 
     private YieldInstruction rocketCooldownInstruction;
+    private YieldInstruction gravityWellCooldownInstruction;
 
     void Awake()
     {
@@ -24,6 +27,7 @@ public class RocketLauncher : MonoBehaviour
 
         transform = GetComponent<Transform>();
         rocketCooldownInstruction = new WaitForSeconds(rocketCooldown);
+        gravityWellCooldownInstruction = new WaitForSeconds(gravityWellCooldown);
     }
 
     void Update()
@@ -32,30 +36,29 @@ public class RocketLauncher : MonoBehaviour
         mouseDir = (Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position).normalized;
 
         // Fire rocket on click
-        bool altFire = Input.GetButton("Alt Fire");
-        bool shootRequested = Input.GetButton("Shoot") || altFire;
-        if (shootRequested && canFireRocket && !PauseMenu.Singleton.IsPaused)
+        if (Input.GetButton("Shoot") && canFireRocket && !PauseMenu.Singleton.IsPaused)
         {
             // Start rocket cooldown
             StartCoroutine(RocketCooldown());
 
             // Spawn rocket
-            if(altFire)
-            {
-                GravityWell gravityWell = PhotonNetwork.Instantiate("GravityWell", transform.position, Quaternion.identity, 0).GetComponent<GravityWell>();
-                gravityWell.IsLocal = true;
-                gravityWell.Direction = mouseDir;
-            } else
-            {
-                Rocket rocket = PhotonNetwork.Instantiate("Rocket", transform.position, Quaternion.FromToRotation(Vector2.right, mouseDir), 0).GetComponent<Rocket>();
-                rocket.IsLocal = true;
-                rocket.Direction = mouseDir;
+            Rocket rocket = PhotonNetwork.Instantiate("Rocket", transform.position, Quaternion.FromToRotation(Vector2.right, mouseDir), 0).GetComponent<Rocket>();
+            rocket.IsLocal = true;
+            rocket.Direction = mouseDir;
 
-                 // Apply recoil force to player
-                playerBody.AddForce(recoilStrength * -mouseDir, ForceMode2D.Impulse);
-            }
+             // Apply recoil force to player
+            playerBody.AddForce(recoilStrength * -mouseDir, ForceMode2D.Impulse);
 
             // Invoke onShoot event
+            onShoot.Invoke();
+        } else if(Input.GetButton("Alt Fire") && canFireGravityWell && !PauseMenu.Singleton.IsPaused)
+        {
+            StartCoroutine(GravityWellCooldown());
+
+            GravityWell gravityWell = PhotonNetwork.Instantiate("GravityWell", transform.position, Quaternion.identity, 0).GetComponent<GravityWell>();
+            gravityWell.IsLocal = true;
+            gravityWell.Direction = mouseDir;
+
             onShoot.Invoke();
         }
     }
@@ -71,6 +74,13 @@ public class RocketLauncher : MonoBehaviour
         canFireRocket = false;
         yield return rocketCooldownInstruction;
         canFireRocket = true;
+    }
+
+    private IEnumerator GravityWellCooldown()
+    {
+        canFireGravityWell = false;
+        yield return gravityWellCooldownInstruction;
+        canFireGravityWell = true;
     }
 
     private void CheckPlayer()
